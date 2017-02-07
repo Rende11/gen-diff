313c0daf5c108cb61aa05a39e755df1b5b63d4cc
rende11 <artamonovn@gmail.com>
2017-02-07 19:50:41 +0300

    -fix: eslint

--- src/compare.js
+++ src/compare.js
@@ -2,13 +2,8 @@
 
 import _ from 'lodash';
 
-const objToArray = (obj) => {
-  const keys = Object.keys(obj);
-  return keys.map(key => ['    ', key, obj[key]]);
-};
-
-
-const arrayToString = array => `{\n${array.map(item => `${item.status}${item.key}: ${item.data}`).join('\n')}\n}`;
+const arrayToString = array => `{\n${array.map(item => `${item.status}
+  ${item.key}: ${item.data}`).join('\n')}\n}`;
 
 const compare = (obj1: Object, obj2: Object) => {
   const keys1 = Object.keys(obj1);
@@ -16,19 +11,19 @@ const compare = (obj1: Object, obj2: Object) => {
 
   const allKeys = _.union(keys1, keys2);
 
-  const setAdded = key => { return {status: '  + ', key: key, data: obj2[key]} };
-  const setStable = key => { return {status: '    ', key, data: obj1[key]} };
-  const setRemoved = key => { return {status: '  - ', key, data: obj1[key]} };
+  const setAdded = key => ({ status: '  + ', key, data: obj2[key] });
+  const setStable = key => ({ status: '    ', key, data: obj1[key] });
+  const setRemoved = key => ({ status: '  - ', key, data: obj1[key] });
 
   const comparedObj = allKeys.map((key) => {
     if (obj1[key] === undefined) {
-      return [setAdded(key)];
+      return setAdded(key);
     }
     if (obj2[key] === undefined) {
-      return [setRemoved(key)];
+      return setRemoved(key);
     }
     if (obj1[key] === obj2[key]) {
-      return [setStable(key)];
+      return setStable(key);
     }
     return [setAdded(key), setRemoved(key)];
   });
