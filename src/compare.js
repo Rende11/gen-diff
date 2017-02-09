cecdbfee961102a37c9b018134603b63d2e4407d
rende11 <artamonovn@gmail.com>
2017-02-09 16:37:39 +0300

    in progress

--- src/compare.js
+++ src/compare.js
@@ -1,23 +1,51 @@
 // @flow
 import _ from 'lodash';
 
-const getAggregate = (agg) => {
-  switch (agg) {
-    case 'added':
-      return '  + ';
-    case 'removed':
-      return '  - ';
-    default:
-      return '    ';
-  }
+// const getAggregate = (agg) => {
+//   switch (agg) {
+//     case 'added':
+//       return '  + ';
+//     case 'removed':
+//       return '  - ';
+//     default:
+//       return '    ';
+//   }
+// };
+
+const statusObj = {
+  added: '  + ',
+  removed: '  - ',
+  stable: '    ',
+  object: 'object',
 };
 
-const render = (array) => {
-  const renderArray = array.map((item) => {
-    const state = getAggregate(item.status);
+const isObject = obj => ((obj instanceof Object) && !(obj instanceof Array));
+// const render = (array) => {
+//   const renderArray = array.map((item) => {
+//     if (item.status === 'object') {
+//       return `${}`;
+//     }
+//     const state = statusObj[item.status];
+//
+//     return `${state}${item.key}: ${item.data}`;
+//   });
+//   return `{\n${renderArray.join('\n')}\n}`;
+// };
+const border = body => `{\n${body}\n}`;
+
+const renderPlainBody = (array) => {
+  const body = array.map((item) => {
+    if (isObject(item.data) && item.status !== 'object') {
+      console.log([item.data].map());
+      return `${statusObj[item.status]}${item.key}:`;
+    }
+    if (item.status === 'object') {
+      return `${statusObj.stable}${item.key}: ${renderPlainBody(item.data)}`;
+    }
+    const state = statusObj[item.status];
     return `${state}${item.key}: ${item.data}`;
-  });
-  return `{\n${renderArray.join('\n')}\n}`;
+  }).join('\n');
+  return border(body);
 };
 
 const compare = (obj1: Object, obj2: Object) => {
@@ -29,6 +57,7 @@ const compare = (obj1: Object, obj2: Object) => {
   const setAdded = key => ({ status: 'added', key, data: obj2[key] });
   const setStable = key => ({ status: 'stable', key, data: obj1[key] });
   const setRemoved = key => ({ status: 'removed', key, data: obj1[key] });
+  const setObject = key => ({ status: 'object', key, data: compare(obj1[key], obj2[key]) });
 
   const comparedObj = allKeys.map((key) => {
     if (obj1[key] === undefined) {
@@ -40,10 +69,13 @@ const compare = (obj1: Object, obj2: Object) => {
     if (obj1[key] === obj2[key]) {
       return setStable(key);
     }
+    if (isObject(obj1[key])) {
+      return setObject(key);
+    }
     return [setAdded(key), setRemoved(key)];
   });
-
-  return render(_.flatten(comparedObj));
+  console.log(_.flatten(comparedObj));
+  return _.flatten(comparedObj);
 };
 
-export default compare;
+export default (obj1, obj2) => renderPlainBody(compare(obj1, obj2));
