d5b357ee5355a6117d19b3f37009a29738bcf23d
rende11 <artamonovn@gmail.com>
2017-03-23 00:22:34 +0300

    -step 5

--- __tests__/index-test.js
+++ __tests__/index-test.js
@@ -34,10 +34,29 @@ describe('Basic structure', () => {
 describe('Complex structure', () => {
   const beforeJSON = '__tests__/fixtures/complexBefore.json';
   const afterJSON = '__tests__/fixtures/complexAfter.json';
+
+  const beforeYAML = '__tests__/fixtures/complexBefore.yaml';
+  const afterYAML = '__tests__/fixtures/complexAfter.yaml';
+
+  const beforeINI = '__tests__/fixtures/complexBefore.ini';
+  const afterINI = '__tests__/fixtures/complexAfter.ini';
+
   const diff = '__tests__/fixtures/expectedComplexJSON.txt';
 
   it('JSON configs', () => {
     const actual = genDiff(beforeJSON, afterJSON);
+    const expected = fs.readFileSync(diff).toString();
+    expect(actual).toBe(expected);
+  });
+
+  it('YAML configs', () => {
+    const actual = genDiff(beforeYAML, afterYAML);
+    const expected = fs.readFileSync(diff).toString();
+    expect(actual).toBe(expected);
+  });
+
+  it('INI configs', () => {
+    const actual = genDiff(beforeINI, afterINI);
     const expected = fs.readFileSync(diff).toString();
     expect(actual).toBe(expected);
   });
